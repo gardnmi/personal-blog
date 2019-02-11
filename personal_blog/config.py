d2a338cd400d87887b6f1ab6e20baf3172058937
gardnmi <gardnmi@gmail.com>
2019-02-10 19:42:44 -0600

    made the configuration for both windows and linux

--- personal_blog/config.py
+++ personal_blog/config.py
@@ -1,11 +1,31 @@
 import os
+import platform
+import json
 
 
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+if platform.system() != 'Windows':
+    with open('etc/config.json') as config_file:
+        config = json.load(config_file)
+        config_exists = True
+else:
+    pass
+
+
+if config_exists:
+    class Config:
+        SECRET_KEY = config.get('SECRET_KEY')
+        SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
+        MAIL_SERVER = 'smtp.googlemail.com'
+        MAIL_PORT = 587
+        MAIL_USE_TLS = True
+        MAIL_USERNAME = config.get('MAIL_USERNAME')
+        MAIL_PASSWORD = config.get('MAIL_PASSWORD')
+else:
+    class Config:
+        SECRET_KEY = os.environ.get('SECRET_KEY')
+        SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+        MAIL_SERVER = 'smtp.googlemail.com'
+        MAIL_PORT = 587
+        MAIL_USE_TLS = True
+        MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+        MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
